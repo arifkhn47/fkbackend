70a9c58a5ab4188ce0c64e576db818ce2bcc7e39
arifkhn46 <arifkhn46@users.noreply.github.com>
2026-06-18 11:29:01 +0530

    Create FoodFactory.php

---
+++ database/factories/FoodFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Food;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Food>
+ */
+class FoodFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'calories' => $this->faker->numberBetween(50, 500),
+            'protein' => $this->faker->randomFloat(2, 0, 50),
+            'carbs' => $this->faker->randomFloat(2, 0, 100),
+            'fats' => $this->faker->randomFloat(2, 0, 30),
+        ];
+    }
+}
