d925e1bd9a229a8a1affc48966cbd709c7b64ca8
arifkhn46 <arifkhn46@users.noreply.github.com>
2026-06-19 13:36:38 +0530

    Use mealable

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Traits\HasMacros;
+use App\Interfaces\Mealable;
 
 #[Fillable(['name', 'calories', 'user_id', 'protein', 'carbs', 'fats'])]
 #[Table('foods')]
-class Food extends Model
+class Food extends Model implements Mealable
 {
-    use HasFactory;
+    use HasFactory, HasMacros;
 }
