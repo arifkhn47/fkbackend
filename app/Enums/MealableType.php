71c4182022f3ac10c70e8a9fb6f16e9d77243a48
arifkhn46 <arifkhn46@users.noreply.github.com>
2026-06-19 13:36:06 +0530

    Method to get all mealables

--- app/Enums/MealableType.php
+++ app/Enums/MealableType.php
@@ -5,4 +5,9 @@ namespace App\Enums;
 enum MealableType: string
 {
     case FOOD = 'food';
+    case RECIPE = 'recipe';
+
+    public static function getAllValues(): array {
+        return array_map(fn($case) => $case->value, self::cases());
+    }
 }
