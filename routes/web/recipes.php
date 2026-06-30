ef061c1fb3427ac7b97bac268ee7d18f02d22c21
Arif Khan <arifkhan@Arifs-iMac.local>
2026-06-30 16:59:01 +0530

    change url to something more meaningful

--- routes/web/recipes.php
+++ routes/web/recipes.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('recipes/create', [RecipeController::class, 'create'])->name('recipes.create')->middleware('auth');
 Route::post('recipes', [RecipeController::class, 'store'])->name('recipes.store')->middleware('auth');
-Route::get('recipe-ingredients/{recipe}/create', [RecipeIngredientController::class, 'create'])
+Route::get('recipes/{recipe}/ingredients/create', [RecipeIngredientController::class, 'create'])
     ->name('recipe.ingredients.create')
     ->middleware('auth');
