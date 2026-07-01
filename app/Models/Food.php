74be7a8437e94c833a801c5d13c3bf3f1a4f1e64
Arif Khan <arifkhan@Arifs-iMac.local>
2026-07-01 14:23:55 +0530

    add search scope to Food

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -8,10 +8,19 @@ use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Traits\HasMacros;
 use App\Interfaces\Mealable;
+use Illuminate\Database\Eloquent\Builder;
 
 #[Fillable(['name', 'calories', 'user_id', 'protein', 'carbs', 'fats'])]
 #[Table('foods')]
 class Food extends Model implements Mealable
 {
     use HasFactory, HasMacros;
+
+    public function scopeSearch(Builder $query, ?string $search): void
+    {
+        $query->when($search, fn ($q, $search) =>
+            $q->where('name', 'like', "%{$search}%")
+        );
+    }
+
 }
